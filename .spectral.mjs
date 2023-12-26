f8c72aff5d50176be5f07fdd77b9357674850699
support@digthisdata.com <support@digthisdata.com>
2023-12-25 20:08:07 -0500

    Deleted 6 files

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjEwNTQ/spectral.js?branch_slug=master&token=954580ba-a3c6-420a-aa37-57509813b1d8";
-export default { extends: ruleset };
